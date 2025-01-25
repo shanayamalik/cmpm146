3601e34f1e783da64de5f599fa02c7d942117897
shanayamalik <shanaya.malik@gmail.com>
2025-01-24 16:17:52 -0800

    called behaviors function correctly, Player 2 winning 5/5 matches

--- P3/behavior_tree_bot/bt_bot.py
+++ P3/behavior_tree_bot/bt_bot.py
@@ -6,47 +6,53 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from behavior_tree_bot.behaviors import *
-from behavior_tree_bot.checks import *
-from behavior_tree_bot.bt_nodes import Selector, Sequence, Action, Check
+# Import specific functions instead of using *
+from checks import (
+    if_neutral_planet_available,
+    have_largest_fleet,
+    is_under_attack,
+    have_strong_fleet,
+    safe_to_spread
+)
+
+from behaviors import (
+    defend_weakest_planet,
+    spread_to_strongest_neutral_planet,
+    attack_enemy_weakpoint,
+    spread_to_closest_neutral_planet
+)
+
+from bt_nodes import Selector, Sequence, Action, Check
 from planet_wars import PlanetWars, finish_turn
 
-# You have to improve this tree or create an entire new one that is capable
-# of winning against all the 5 opponent bots
 def setup_behavior_tree():
-   # Top-down construction of behavior tree
-   root = Selector(name='High Level Strategy')
+    root = Selector(name='High Level Strategy')
 
-   # Added defensive strategy to protect our planets
-   defensive_plan = Sequence(name='Defensive Strategy')
-   under_attack = Check(is_under_attack)
-   defend = Action(defend_weakest_planet)
-   defensive_plan.child_nodes = [under_attack, defend]
+    defensive_plan = Sequence(name='Defensive Strategy')
+    under_attack = Check(is_under_attack)
+    defend = Action(defend_weakest_planet)
+    defensive_plan.child_nodes = [under_attack, defend]
 
-   # Modified production strategy to target valuable planets
-   production_plan = Sequence(name='Production Strategy')
-   neutral_available = Check(if_neutral_planet_available)
-   strongest_neutral = Action(spread_to_strongest_neutral_planet)
-   production_plan.child_nodes = [neutral_available, strongest_neutral]
+    production_plan = Sequence(name='Production Strategy')
+    neutral_available = Check(if_neutral_planet_available)
+    strongest_neutral = Action(spread_to_strongest_neutral_planet)
+    production_plan.child_nodes = [neutral_available, strongest_neutral]
 
-   # New aggressive strategy when we have superior forces
-   aggressive_plan = Sequence(name='Aggressive Strategy')
-   strong_fleet = Check(have_strong_fleet)
-   strong_attack = Action(attack_enemy_weakpoint)
-   aggressive_plan.child_nodes = [strong_fleet, strong_attack]
+    aggressive_plan = Sequence(name='Aggressive Strategy')
+    strong_fleet = Check(have_strong_fleet)
+    strong_attack = Action(attack_enemy_weakpoint)
+    aggressive_plan.child_nodes = [strong_fleet, strong_attack]
 
-   # Safe expansion as fallback
-   fallback_plan = Sequence(name='Fallback Strategy')
-   safe_to_spread = Check(safe_to_spread)
-   spread = Action(spread_to_closest_neutral_planet)
-   fallback_plan.child_nodes = [safe_to_spread, spread]
+    fallback_plan = Sequence(name='Fallback Strategy')
+    safe_to_spread_check = Check(safe_to_spread)  # Renamed to avoid naming conflict
+    spread = Action(spread_to_closest_neutral_planet)
+    fallback_plan.child_nodes = [safe_to_spread_check, spread]
 
-   root.child_nodes = [defensive_plan, production_plan, aggressive_plan, fallback_plan]
-   
-   logging.info('\n' + root.tree_to_string())
-   return root
+    root.child_nodes = [defensive_plan, production_plan, aggressive_plan, fallback_plan]
 
-# You don't need to change this function
+    logging.info('\n' + root.tree_to_string())
+    return root
+
 def do_turn(state):
    behavior_tree.execute(planet_wars)
 
