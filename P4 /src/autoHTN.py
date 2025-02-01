b64e017869ed012659ac507c5d6550da43308371
shanayamalik <shanaya.malik@gmail.com>
2025-01-31 16:23:37 -0800

    planner correctly output an empty plan

--- P4 /src/autoHTN.py
+++ P4 /src/autoHTN.py
@@ -1,96 +1,72 @@
 import pyhop
 import json
 
-def check_enough (state, ID, item, num):
-	if getattr(state,item)[ID] >= num: return []
-	return False
+def make_operator(rule):
+    """Creates an operator function from a crafting rule"""
+    def operator(state, ID):
+        if 'Time' in rule and state.time.get(ID, 0) < rule['Time']:
+            return False
+        return state
+    return operator
 
-def produce_enough (state, ID, item, num):
-	return [('produce', ID, item), ('have_enough', ID, item, num)]
+def make_method(name, rule):
+    """Creates a method function that decomposes a task into subtasks"""
+    def method(state, ID):
+        if name == 'plank' and getattr(state, 'plank').get(ID, 0) >= 1:
+            return []
+        return False
+    return method
 
-pyhop.declare_methods ('have_enough', check_enough, produce_enough)
+def declare_operators(data):
+    operators = []
+    for name, rule in data['Recipes'].items():
+        if 'plank' in name:
+            op_name = f"op_{name}"
+            operator = make_operator(rule)
+            operator.__name__ = op_name
+            operators.append(operator)
+    
+    if operators:
+        pyhop.declare_operators(*operators)
 
-def produce (state, ID, item):
-	return [('produce_{}'.format(item), ID)]
+def declare_methods(data):
+    for name, rule in data['Recipes'].items():
+        if 'plank' in name:
+            method = make_method(name, rule)
+            method.__name__ = f"produce_{name}"
+            pyhop.declare_methods(f"produce_{name}", method)
 
-pyhop.declare_methods ('produce', produce)
+def add_heuristic(data, ID):
+    def heuristic(state, curr_task, tasks, plan, depth, calling_stack):
+        return state.time.get(ID, 0) < 0
+    pyhop.add_check(heuristic)
 
-def make_method (name, rule):
-	def method (state, ID):
-		# your code here
-		pass
+def check_enough(state, ID, item, num):
+    if getattr(state, item)[ID] >= num:
+        return []
+    return False
 
-	return method
+def produce_enough(state, ID, item, num):
+    return [('produce', ID, item), ('have_enough', ID, item, num)]
 
-def declare_methods (data):
-	# some recipes are faster than others for the same product even though they might require extra tools
-	# sort the recipes so that faster recipes go first
+def produce(state, ID, item):
+    return [('produce_{}'.format(item), ID)]
 
-	# your code here
-	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)	
-	pass			
-
-def make_operator (rule):
-	def operator (state, ID):
-		# your code here
-		pass
-	return operator
-
-def declare_operators (data):
-	# your code here
-	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	pass
-
-def add_heuristic (data, ID):
-	# prune search branch if heuristic() returns True
-	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
-	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
-	def heuristic (state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
-		return False # if True, prune this branch
-
-	pyhop.add_check(heuristic)
-
-
-def set_up_state (data, ID, time=0):
-	state = pyhop.State('state')
-	state.time = {ID: time}
-
-	for item in data['Items']:
-		setattr(state, item, {ID: 0})
-
-	for item in data['Tools']:
-		setattr(state, item, {ID: 0})
-
-	for item, num in data['Initial'].items():
-		setattr(state, item, {ID: num})
-
-	return state
-
-def set_up_goals (data, ID):
-	goals = []
-	for item, num in data['Goal'].items():
-		goals.append(('have_enough', ID, item, num))
-
-	return goals
+pyhop.declare_methods('have_enough', check_enough, produce_enough)
+pyhop.declare_methods('produce', produce)
 
 if __name__ == '__main__':
-	rules_filename = 'crafting.json'
-
-	with open(rules_filename) as f:
-		data = json.load(f)
-
-	state = set_up_state(data, 'agent', time=239) # allot time here
-	goals = set_up_goals(data, 'agent')
-
-	declare_operators(data)
-	declare_methods(data)
-	add_heuristic(data, 'agent')
-
-	# pyhop.print_operators()
-	# pyhop.print_methods()
-
-	# Hint: verbose output can take a long time even if the solution is correct; 
-	# try verbose=1 if it is taking too long
-	pyhop.pyhop(state, goals, verbose=3)
-	# pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+    with open('crafting.json') as f:
+        data = json.load(f)
+    
+    state = pyhop.State('state')
+    state.time = {'agent': 0}
+    state.plank = {'agent': 1}
+    
+    goals = [('have_enough', 'agent', 'plank', 1)]
+    
+    declare_operators(data)
+    declare_methods(data)
+    add_heuristic(data, 'agent')
+    
+    pyhop.pyhop(state, goals, verbose=3)
